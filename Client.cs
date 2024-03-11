4c7021321d7cab2445d12eaf0f35bd37cb9c6999
Trabatu <kluge.lukasz@gmail.com>
2024-03-11 19:05:21 +0100

    test

---
+++ Client.cs
@@ -0,0 +1,20 @@
+﻿using System.Security;
+
+namespace ABC.BL
+{
+    public class Client
+    {
+        public string? Surname { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public int ClientID { get; private set; }
+
+        public string? NameSurname
+        {
+            get
+            {
+                return Name + ", " + Surname;
+            }
+        }
+    }
+}
