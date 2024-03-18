525a1e2e86067ac858d14dc419c4e89dc47a6bf6
Trabatu <kluge.lukasz@gmail.com>
2024-03-18 09:52:50 +0100

    Save, Get client methods

--- Client.cs
+++ Client.cs
@@ -39,7 +39,25 @@ namespace ABC.BL
                 correct = false;
 
             return correct;
-            
+
+        }
+
+        // save current client
+        public bool Save()
+        {
+            return true;
+        }
+
+        //download new client
+        public Client Get(int ClientID)
+        {
+            return new Client();
+        }
+
+        public List<Client> GetClients() 
+        {   
+            // get all the clients
+            return new List<Client>();
         }
     }
 }
