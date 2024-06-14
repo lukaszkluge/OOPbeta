e1e7b616c5684eb25d8f363f1db5f90022950686
Trabatu <kluge.lukasz@gmail.com>
2024-06-14 10:39:17 +0200

    Order class

--- Order.cs
+++ Order.cs
@@ -15,7 +15,36 @@ namespace ABC.BL
 
         public Order(int orderID)
         {
-            orderID = orderID;
+            OrderID = orderID;
+        }
+
+        public int OrderID { get; private set; }
+        public DateTimeOffset? OrderDate { get; set; }
+
+
+        // get one order
+        public Order Get(int orderID)
+        {
+            //code that provides defined product
+            return new Order();
+        }
+
+        // save current order
+        public bool Save()
+        {
+            // code that saves defined order
+            return true;
+        }
+
+        // check order data
+        public bool Validate()
+        {
+            var correct = true;
+
+            if (OrderDate == null)
+                correct = false;
+
+            return correct;
         }
     }
 }
