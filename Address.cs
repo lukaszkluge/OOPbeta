c83c7b5288970392ae3a35f7c03478ef22457da3
Łukasz Kluge <lukasz@fakt.com.pl>
2024-07-02 14:55:40 +0200

    Address class

---
+++ Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC.BL
+{
+    internal class Address
+    {
+        public class Address
+        {
+            public Address()
+            {
+                
+            }
+
+            public Address(int addressID)
+            {
+                AddressID = addressID;
+            }
+
+            public int AddressID { get; private set; }
+        }
+    }
+}
